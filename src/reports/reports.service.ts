f6448570469ec0fe24aceea3392c600cc9b3cc91
Tornike <goart2000@gmail.com>
2022-11-07 17:58:25 +0400

    query builder

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,11 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/users/user.schema';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './interface/report.interface';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectModel('Report') private repo: Model<Report>) {}
+
+  createEstimate({ make, model, lng, lat }: GetEstimateDto) {
+    return this.repo
+      .find()
+      .where({ make })
+      .where({ model })
+      .where({ lng })
+      .where({ lat });
+  }
 
   async create(reportDto: CreateReportDto, user: User) {
     const report = new this.repo({
